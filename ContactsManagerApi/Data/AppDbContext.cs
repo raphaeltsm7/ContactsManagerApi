d2f0c993eada38f17b4544887a575b0ecca3b5a9
Raphael Magalhães <raphaelsoares11@hotmail.com>
2023-06-14 22:20:18 -0300

    - Criei a classe Contact para representar um contato no sistema. - Configurei o DbContext para trabalhar com o banco de dados MySQL. - Atualizei o arquivo README.md com as instruções de configuração e execução do projeto. - Adicionei o arquivo .gitignore com as configurações recomendadas para projetos C#.

---
+++ ContactsManagerApi/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using ContactsManagerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsManagerApi.Data {
+    public class AppDbContext : DbContext {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        }
+    }
+}
---
+++ ContactsManagerApi/Program.cs
@@ -0,0 +1,27 @@
+using ContactsManagerApi.Data;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => {
+    options.UseSqlServer(Configuration.GetConnectionString("NomeDaSuaConexao"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
