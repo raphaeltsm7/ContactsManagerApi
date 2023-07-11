7221cd351ccfe27aa0d5f673e4ff45a904f71874
Raphael Magalhães <raphaelsoares11@hotmail.com>
2023-07-11 20:52:25 -0300

    Editado layou home, adicionado imagem e edições visuais

--- ContactsManagerWeb/Controllers/HomeController.cs
+++ ContactsManagerWeb/Controllers/HomeController.cs
@@ -1,29 +1,33 @@
-﻿using ContactsManagerWeb.Models;
+﻿using AutoMapper;
+using ContactsManagerWeb.Extensions;
+using ContactsManagerWeb.Models;
 using ContactsManagerWeb.Models.DTO;
+using ContactsManagerWeb.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace ContactsManagerWeb.Controllers {
     public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IContactsService _contactsService;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
+        public HomeController(IContactsService contactsService, IMapper mapper)
+        {
+            _contactsService = contactsService;
+            _mapper = mapper;
         }
 
-        public IActionResult Index() {
-
-            return View();
+        public async Task<IActionResult> Index()
+        {
+            var listContacts = new List<ContactsDTO>();
+            var response = await _contactsService.GetAllAsync<APIResponse>();
+            if (response.IsValid())
+            {
+                listContacts = JsonConvert.DeserializeObject<List<ContactsDTO>>(Convert.ToString(response.Result));
+            }
+            return View(listContacts);
         }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
         }
     }
-}
