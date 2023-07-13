66263c384ce3caf66e6134e67d8c8d0dc321e6d1
Raphael Magalhães <raphaelsoares11@hotmail.com>
2023-07-12 22:19:16 -0300

    Implementando registro de usuário

---
+++ ContactsManagerApi/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ContactsManagerApi.Models;
+using ContactsManagerApi.Models.DTO;
+
+namespace ContactsManagerApi.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO>Login(LoginResquestDTO loginResquestDTO);
+        Task<LocalUser>Register(RegistrationResquestDTO registrationResquestDTO);
+        
+
+    }
+}
---
+++ ContactsManagerApi/Repository/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using ContactsManagerApi.Models.DTO;
+using ContactsManagerApi.Models;
+using ContactsManagerApi.Repository.IRepository;
+using ContactsManagerApi.Data;
+
+namespace ContactsManagerApi.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ContactsDbContext _db;
+        public UserRepository(ContactsDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var  user =  _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<LoginResponseDTO> Login(LoginResquestDTO loginResquestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegistrationResquestDTO registrationResquestDTO)
+        {
+            LocalUser user = new LocalUser()
+            {
+                UserName = registrationResquestDTO.UserName,
+                Password = registrationResquestDTO.Password,
+                Name = registrationResquestDTO.Name,
+                Role = registrationResquestDTO.Role
+            };
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
+
