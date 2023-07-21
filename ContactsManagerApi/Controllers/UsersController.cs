68a8f85a4784c3d665a1e75c53e878bd9475b7b5
Raphael Magalhães <raphaelsoares11@hotmail.com>
2023-07-20 23:19:49 -0300

    Ajustes na autenticação do usuário e retorno de login OK

--- ContactsManagerApi/Controllers/UsersController.cs
+++ ContactsManagerApi/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
-﻿using ContactsManagerApi.Models.DTO;
+﻿using ContactsManagerApi.Models;
+using ContactsManagerApi.Models.DTO;
 using ContactsManagerApi.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ContactsManagerApi.Controllers
 {
@@ -9,18 +11,26 @@ namespace ContactsManagerApi.Controllers
     public class UsersController : Controller
     {
         private readonly IUserRepository _userRepo;
+        protected APIResponse _response;
         public UsersController(IUserRepository userRepo)
         {
             _userRepo = userRepo;
+            this._response = new();
         }
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginResquestDTO model)
         {
             var loginResponse = await _userRepo.Login(model);
             if(loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token)) {
-                return BadRequest(new { message = "Username or password is incorrect." });
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username or password is incorrect." );
+                return BadRequest(_response);
             }
-            return View();
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = loginResponse;
+            return Ok(_response);
         }
 
         [HttpPost("register")]
